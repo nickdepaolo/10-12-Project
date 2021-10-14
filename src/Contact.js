a7e08e676934dd9a457850de41dd9d7e224364c2
nickdepaolo <79952881+nickdepaolo@users.noreply.github.com>
2021-10-14 15:53:29 -0400

    10/12 1553

--- src/Contact.js
+++ src/Contact.js
@@ -1,31 +1,55 @@
 import React, {useState} from 'react';
-import { Row } from 'reactstrap';
+// import { Row } from 'reactstrap';
 
 const Contact = (props) => {
 
     const APIURL = 'https://rickandmortyapi.com/api/';
-    const [character, setCharacter] = useState('');
-    const [location, setLocation] = useState('');
-    const [episode, setEpisode] = useState('')
+    const [character, setCharacter] = useState('Rick');
+    const [location, setLocation] = useState('Earth (C-137)');
+    const [episode, setEpisode] = useState('The Ricklantis Mixup');
+
+    const submitCharacter = (event) => {
+        fetch(APIURL+`character/?name=`+character)
+            .then((res) => res.json())
+            .then((props) =>{
+                console.log(props)
+            })
+    }
+
+    const submitLocation = (event) => {
+        fetch(APIURL+`location/?name=`+location)
+            .then((res) => res.json())
+            .then((props) =>{
+                console.log(props)
+            })
+    }
+
+    const submitEpisode = (event) => {
+        fetch(APIURL+`episode/?name=`+character)
+            .then((res) => res.json())
+            .then((props) =>{
+                console.log(props)
+            })
+    }
 
     return(
         <div>
             <h3>Character</h3>
             <input onChange={(e) => setCharacter(e.target.value)} name="Character" />
             <br/>
-            <button type="button">Search</button>
+            <button type="button" onClick={submitCharacter} >Search</button>
             <br/>
             
             <h3>Location</h3>
             <input onChange={(e) => setLocation(e.target.value)} name="Location" />
             <br/>
-            <button type="button">Search</button>
+            <button type="button" onClick={submitLocation}>Search</button>
             <br/>
           
             <h3>Episode</h3>
             <input onChange={(e) => setEpisode(e.target.value)} name="Episode" />
             <br/>
-            <button type="button">Search</button>
+            <button type="button" onClick={submitEpisode}>Search</button>
             {console.log(character, location, episode)}
         </div>
     )
