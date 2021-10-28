266ecfd8185ead7f9e049b53755a79a7e46e870c
nickdepaolo <79952881+nickdepaolo@users.noreply.github.com>
2021-10-27 22:37:10 -0400

    10/27 2237

--- src/components/Character/CharacterMap.js
+++ src/components/Character/CharacterMap.js
@@ -6,23 +6,51 @@ import '../Components.css'
 
 const CharacterMap = (props) => {
     
-    let infoContain = props.infoContain;
-    const sliceChar = infoContain.slice(0,1)
+    const infoContain = props.infoContain;
     const sliceList = infoContain.slice(1,22)
+    const sliceChar = infoContain.slice(0,1)
+    const [passArray, setPassArray] = useState([])
+    const [spotState, setSpotState] = useState([])
+    const [isTrue, setIsTrue] = useState(false)
 
+    useEffect(() =>{
+        setIsTrue(false)
+    }, [props.infoContain])
+
+
+    function setTrue() {
+        setIsTrue(true);
+        console.log(isTrue)
+    }
+    
+    function firstSet() {
+        setSpotState(sliceChar)
+    }
+    
     function passProps() {
         props.triggerInput()
+    }
+    
+    function setSliceArray(e) {
+        let sliceArray = []
+        sliceArray.push(e)
+        setPassArray(sliceArray)
+    }
+    
+    function setSliceState() {
         window.scrollTo({top:0, left:0, behavior: 'smooth'})
+        setSpotState(passArray)
+        setTrue()
     }
     
     return(
         <div>
    
-            <CharacterSpotlight spotContain={sliceChar}/>
+            <CharacterSpotlight spotContain={isTrue? passArray : sliceChar}/>
         
             {sliceList.map((card) => (
-                <div key={card.id} id={card.id} value={card.name} onMouseOver={(e) => props.inputSelect(card.name)} onClick={passProps}>
-                    <img width='150em' alt='' src={card.image} value={card.name}/>
+                <div key={card.id} id={card.id} value={card.name} onClick={() => setSliceArray(card)} >
+                    <img width='150em' alt='' src={card.image} value={card.name} onClick={setSliceState}/>
                     <h3>{card.name}</h3>
                     <p>{card.location.name}</p>
                     <p>{card.gender}</p>
