3f946d3d864669b9d7a5348a18f80d02af0b9de3
nickdepaolo <79952881+nickdepaolo@users.noreply.github.com>
2022-01-27 20:13:25 -0500

    2013 1/27

--- src/components/Failure.js
+++ src/components/Failure.js
@@ -7,6 +7,8 @@ const Failure = () => {
         <div>
             <p>Search crashes if it fails</p>
             <p>SetState for adv search could be better</p>
+            <p>More needs set up in location search</p>
+            <p>More needs set up in episode search</p>
         </div>
     )
 }
