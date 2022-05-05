f5a11f7508cffce859c4fdd9b2fc70a2ec7ae35e
nickdepaolo <79952881+nickdepaolo@users.noreply.github.com>
2022-05-05 04:36:24 -0400

    0505 0436

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
         <div id="footer">
             
             <div id="footerTextWrapper">
-                <a className="footerText" href="https://www.linkedin.com/in/nick-de-paolo-a57788237/">Nicholas De Paolo 2022</a>
+                <a className="footerText" href="https://www.linkedin.com/in/nick-de-paolo-a57788237/">Nicholas De Paolo</a>
                 <a className="footerText" href="https://rickandmortyapi.com/">Rick and Morty API</a>
                 <a className="footerText" href="https://github.com/nickdepaolo/10-12-Project">GitHub Repo</a>
             </div>
